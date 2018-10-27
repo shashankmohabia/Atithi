23765c74435a073c145a6c2a51464ae12121c097
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-10-27 14:58:58 +0530

    create thread in BootActivity

--- app/src/main/java/com/example/shashankmohabia/atithi/BootActivity.kt
+++ app/src/main/java/com/example/shashankmohabia/atithi/BootActivity.kt
@@ -1,12 +1,28 @@
 package com.example.shashankmohabia.atithi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class BootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boot)
+
+        val intent = Intent(this, MainActivity::class.java)
+        Thread{
+            try {
+                sleep(3000)
+            }
+            catch (e:InterruptedException){
+                e.printStackTrace()
+            }
+            finally {
+                startActivity(intent)
+                finish()
+            }
+        }.start()
     }
 }
