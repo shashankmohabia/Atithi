b767957fb47b2ee1e9ca26dee545162f814d9b7c
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-10-28 10:59:12 +0530

    add bottom navbar in MainActivity

--- app/src/main/java/com/example/shashankmohabia/atithi/Core/MainActivity.kt
+++ app/src/main/java/com/example/shashankmohabia/atithi/Core/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.shashankmohabia.atithi.Core
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -19,10 +18,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        /*fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
+        }*/
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
