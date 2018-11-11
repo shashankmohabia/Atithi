5264956c9c8a1addfcf5bf43e9b3067514703d46
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-11-11 17:46:20 +0530

    create server model classes

---
+++ app/src/main/java/com/example/shashankmohabia/atithi/Servers/Place.kt
@@ -0,0 +1,12 @@
+package com.example.shashankmohabia.atithi.Servers
+
+data class Place(
+        val name: String,
+        val city: String,
+        val state: String,
+        val country: String,
+        val description: String,
+        val image_link: String,
+        val opening_time: String,
+        val closing_time: String
+)
---
+++ app/src/main/java/com/example/shashankmohabia/atithi/Servers/SubPlace.kt
@@ -0,0 +1,4 @@
+package com.example.shashankmohabia.atithi.Servers
+
+class SubPlace {
+}
