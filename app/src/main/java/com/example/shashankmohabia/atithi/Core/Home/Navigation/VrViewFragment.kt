d6d71e1805b46ad101d53e049cbe33a0b916c130
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-11-24 01:39:43 +0530

    modify VrViewFragment using Glide to inflate image

--- app/src/main/java/com/example/shashankmohabia/atithi/Core/Home/Navigation/VrViewFragment.kt
+++ app/src/main/java/com/example/shashankmohabia/atithi/Core/Home/Navigation/VrViewFragment.kt
@@ -1,73 +1,68 @@
 package com.example.shashankmohabia.atithi.Core.Home.Navigation
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.shashankmohabia.atithi.R
 import com.google.vr.sdk.widgets.pano.VrPanoramaView
 import android.support.annotation.Nullable
-import kotlinx.android.synthetic.main.navigation_main.*
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+import kotlinx.android.synthetic.main.vr_view_fragment.*
 
 class VrViewFragment : Fragment() {
-    lateinit var panoWidgetView: VrPanoramaView
-    private var backgroundImageLoaderTask: ImageLoaderTask? = null
 
+    companion object {
+        private const val VR_IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/atithi-220608.appspot.com/o/Places%2FMehrangarh_Fort-Jodhpur%2FSubPlaces%2FRang%20Mehal_360.jpg?alt=media&token=adde429c-0bb3-4b69-9122-19479a8c1a22"
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val v = inflater.inflate(R.layout.vr_view_fragment, container, false)
-        panoWidgetView = v.findViewById(R.id.vr_view)
-        return v
+        return inflater.inflate(R.layout.vr_view_fragment, container, false)
     }
+    /*
 
     override fun onPause() {
-        panoWidgetView.pauseRendering()
+        vr_view.pauseRendering()
         super.onPause()
     }
 
     override fun onResume() {
-        panoWidgetView.resumeRendering()
+        vr_view.resumeRendering()
         super.onResume()
     }
 
     override fun onDestroy() {
         // Destroy the widget and free memory.
-        panoWidgetView.shutdown()
+        vr_view.shutdown()
         super.onDestroy()
-    }
-
-    @Synchronized
-    private fun loadPanoImage() {
-        var task = backgroundImageLoaderTask
-        if (task != null && !task.isCancelled()) {
-            // Cancel any task from a previous loading.
-            task.cancel(true)
-        }
-
-        // pass in the name of the image to load from assets.
-        val viewOptions = VrPanoramaView.Options()
-        viewOptions.inputType = VrPanoramaView.Options.TYPE_MONO
-
-        // use the name of the image in the assets/ directory.
-        val panoImageName = "PANO_20181118_141826_0.jpg"
-
-        // create the task passing the widget view and call execute to start.
-        task = ImageLoaderTask(panoWidgetView, viewOptions, panoImageName)
-        task.execute(activity!!.assets)
-        backgroundImageLoaderTask = task
-    }
+    }*/
 
     override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        loadPanoImage()
+        vr_view!!.apply {
+            setFlingingEnabled(true)
+            setInfoButtonEnabled(true)
+            setStereoModeButtonEnabled(true)
+            setFullscreenButtonEnabled(true)
+        }
+        loadContent()
+    }
+
+    private fun loadContent() {
+        Glide.with(this)
+                .asBitmap()
+                .load(VR_IMAGE_URL)
+                .into(object : SimpleTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        val options = VrPanoramaView.Options()
+                        options.inputType = VrPanoramaView.Options.TYPE_MONO
+                        vr_view!!.loadImageFromBitmap(resource, options)
+                    }
+                })
     }
 }
 
